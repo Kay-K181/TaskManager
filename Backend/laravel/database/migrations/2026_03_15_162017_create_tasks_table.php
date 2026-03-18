c6805a46a88879f14a4f589b51151a28774b1f58
kalendwilliams <kaykaywill@live.co.uk>
2026-03-18 16:05:16 +0000

    update

--- Backend/laravel/database/migrations/2026_03_15_162017_create_tasks_table.php
+++ Backend/laravel/database/migrations/2026_03_15_162017_create_tasks_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('title');
             $table->text('description')->nullable();
             $table->boolean('completed')->default(false);
-            $table->foreignId('user_id')->constrained()->onDelete('cascade'); // link to users
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
